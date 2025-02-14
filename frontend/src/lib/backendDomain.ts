9027414bad7898532983930d75db7355817b47a3
Brian Siao Tick Chong <bsiaotickchong@gmail.com>
2025-02-14 11:13:42 -0800

    Fix backendDomain for production builds

--- frontend/src/lib/backendDomain.ts
+++ frontend/src/lib/backendDomain.ts
@@ -1,16 +1,3 @@
-import assert from "assert";
-import isDevelopment from "./isDevelopment";
-
 export const getBackendDomain = () => {
-  if (typeof window !== "undefined") {
-    if (isDevelopment) {
-      assert(
-        process.env.NEXT_PUBLIC_BACKEND_DOMAIN,
-        "NEXT_PUBLIC_BACKEND_DOMAIN environment variable must be set in development mode.",
-      );
-      return process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
-    }
-
-    return window.location.host;
-  }
+  return process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
 };
