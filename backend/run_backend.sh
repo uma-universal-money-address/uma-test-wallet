10d4693484206e42fc5ed03740769348d3e9ecbb
Peng Ying <peng@lightspark.com>
2025-03-04 15:51:57 -0800

    changing port to 4000 (to fix potential airplay issues) and tweaking host

--- backend/run_backend.sh
+++ backend/run_backend.sh
@@ -3,6 +3,6 @@ export FLASK_CONFIG="config/local-dev.py"
 export FLASK_APP=vasp
 export FLASK_ENV=development
 export FLASK_DEBUG=True
-export FLASK_RUN_PORT=5000
+export FLASK_RUN_PORT=4000
 
-pipenv run flask run
+pipenv run flask run --host=0.0.0.0
