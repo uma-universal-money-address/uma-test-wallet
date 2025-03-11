654512865e0ed84ac19cfd7a6be232c0d9d9c595
Peng Ying <peng@lightspark.com>
2025-03-11 16:27:27 -0700

    updating the index number (#94)

--- frontend/src/app/uma-settings/page.tsx
+++ frontend/src/app/uma-settings/page.tsx
@@ -20,9 +20,9 @@ export default function Page() {
   const [isDeleteUmaOpen, setIsDeleteUmaOpen] = useState(false);
   const { wallets, isLoading: isLoadingWallets, fetchWallets } = useWallets();
 
-  const currWalletIndex = wallets?.findIndex(
-    (wallet) => wallet.id === currentWallet?.id,
-  );
+  const currWalletIndex = wallets
+    ? wallets?.findIndex((wallet) => wallet.id === currentWallet?.id) + 1
+    : undefined;
 
   const handleDeleteUma = async () => {
     if (currentWallet) {
