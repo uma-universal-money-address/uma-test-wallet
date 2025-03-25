dd14a655716bf58bc74956831e370049a55177e8
Brian Siao Tick Chong <bsiaotickchong@gmail.com>
2025-03-25 15:02:40 -0700

    [frontend] use backend domain env var for dev only

--- frontend/src/lib/backendDomain.ts
+++ frontend/src/lib/backendDomain.ts
@@ -1,10 +1,13 @@
+import isDevelopment from "./isDevelopment";
+
 export const getBackendDomain = () => {
-  if (typeof window !== "undefined") {
-    return (
-      (process.env.NEXT_PUBLIC_BACKEND_DOMAIN &&
-        new URL(process.env.NEXT_PUBLIC_BACKEND_DOMAIN).host) ||
-      window.location.host
-    );
+  if (isDevelopment) {
+    const backendDomain = process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
+    return backendDomain && new URL(backendDomain).host;
+  } else if (typeof window !== "undefined") {
+    return window.location.host;
   }
+
+  // Should never happen, here for type safety during nextjs build
   return "";
 };
