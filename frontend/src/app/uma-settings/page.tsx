31f690449ae82907cc2c81a36f65436694d01116
Jeremy Klein <jklein24@gmail.com>
2025-04-10 23:40:33 -0700

    Also fix nwc connection link

--- frontend/src/app/uma-settings/page.tsx
+++ frontend/src/app/uma-settings/page.tsx
@@ -50,7 +50,7 @@ export default function Page() {
       title: "Manage connected apps",
       action: {
         type: "external-link",
-        href: `${isDevelopment ? "http" : "https"}://${getBackendDomain()}/nwc`,
+        href: `${isDevelopment ? "http" : "https"}://${getBackendDomain()}/nwc/`,
       },
       description: "View your UMA Auth connections",
     },
