029909488cc11bfccb2b7d5e25d98891fccea3d0
Peng Ying <peng@lightspark.com>
2025-10-20 11:37:22 -0700

    fixing amount

--- backend/vasp/uma_vasp/receiving_vasp.py
+++ backend/vasp/uma_vasp/receiving_vasp.py
@@ -355,7 +355,7 @@ class ReceivingVasp:
             receiver_fees_msats=(
                 0
                 if request.receiving_currency_code in ["SAT", "MXN"]
-                else 2_000 if request.receiving_currency_code is not None else None
+                else 250_000 if request.receiving_currency_code is not None else None
             ),
             receiver_node_pubkey=None,
             receiver_utxos=[],
