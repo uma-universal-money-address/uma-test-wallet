867f5a950906bd799d13314acd4493252666f0e8
Peng Ying <peng@lightspark.com>
2025-12-04 14:36:31 -0800

    updating for black

--- backend/alembic/versions/e9410c3bf621_backfilling_all_accounts_to_be_user_.py
+++ backend/alembic/versions/e9410c3bf621_backfilling_all_accounts_to_be_user_.py
@@ -5,13 +5,14 @@ Revises: 115fd8d013a7
 Create Date: 2025-12-04 14:24:04.522611
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'e9410c3bf621'
-down_revision: Union[str, None] = '115fd8d013a7'
+revision: str = "e9410c3bf621"
+down_revision: Union[str, None] = "115fd8d013a7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
